06b5678b14e1987d99cfa4968569d716c855f1c7
katerinabuhalova <katerina.buhalova@gmail.com>
2021-05-27 14:55:47 +0300

    implement main scrin off the app.

--- app/src/main/java/com/example/megokinopoisk/MainActivity.kt
+++ app/src/main/java/com/example/megokinopoisk/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.megokinopoisk
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.megokinopoisk.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,16 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dashboard, menu)
+
+        val searchItem: MenuItem? = menu?.findItem(R.id.action_search)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView: SearchView? = searchItem?.actionView as SearchView
+
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return super.onCreateOptionsMenu(menu)
     }
 }
